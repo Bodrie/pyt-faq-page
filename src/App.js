f5fdf2f333fed91ad152a0871aaaae750a3a20a6
Dobromir Kirov <kirov0407@gmail.com>
2021-06-18 11:44:50 +0300

    Header style update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
-const Header = () => {
+import './Header.css';
+
+function Header() {
 
     return (
         <header>
-            <h1>Frequently Asked Questions</h1>
+            <h1 className="header-h1">Frequently Asked Questions</h1>
         </header>
     );
 
